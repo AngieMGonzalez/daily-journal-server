69633ea7b07ef95f34c3a611131c5499458951f9
Angie Gonzalez <angiem.go@gmail.com>
2023-04-24 01:23:37 -0500

    created tables for ch1

--- request_handler.py
+++ request_handler.py
@@ -27,9 +27,9 @@ class HandleRequests(BaseHTTPRequestHandler):
 
         # It's an if..else statement
         if self.path == "/entries":
-          response = get_all_entries()
+            response = get_all_entries()
         else:
-          response = []
+            response = []
 
         # Send a JSON formatted string as a response
         self.wfile.write(json.dumps(response).encode())
@@ -72,9 +72,9 @@ class HandleRequests(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Access-Control-Allow-Methods',
-                          'GET, POST, PUT, DELETE')
+                        'GET, POST, PUT, DELETE')
         self.send_header('Access-Control-Allow-Headers',
-                          'X-Requested-With, Content-Type, Accept')
+                        'X-Requested-With, Content-Type, Accept')
         self.end_headers()
 
 
--- views/mood_requests.py
+++ views/mood_requests.py
@@ -1,24 +1,7 @@
 MOODS = [
     {
         "id": 1,
-        "name": "S",
-        "species": "D",
-        "locationId": 1,
-        "customerId": 4
-    },
-    {
-        "id": 2,
-        "name": "R",
-        "species": "D",
-        "locationId": 1,
-        "customerId": 2
-    },
-    {
-        "id": 3,
-        "name": "B",
-        "species": "C",
-        "locationId": 2,
-        "customerId": 1
+        "label": "Dogs"
     }
 ]
 
