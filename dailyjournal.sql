23e34c9d8d622994964a45a1716afc83a6e17b01
Angie Gonzalez <angiem.go@gmail.com>
2023-04-24 09:53:21 -0500

    get all entries works in SQL and Postman

--- dailyjournal.sql
+++ dailyjournal.sql
@@ -23,20 +23,15 @@ INSERT INTO `Mood` VALUES (null, "confused");
 INSERT INTO `Mood` VALUES (null, "angry");
 INSERT INTO `Mood` VALUES (null, "love");
 INSERT INTO `Mood` VALUES (null, "embarrassed");
-INSERT INTO `Mood` VALUES (null, "silly");INSERT INTO `Mood` VALUES (null, "sick");
+INSERT INTO `Mood` VALUES (null, "silly");
+INSERT INTO `Mood` VALUES (null, "sick");
 INSERT INTO `Mood` VALUES (null, "surprised");
 INSERT INTO `Mood` VALUES (null, "nervous");
 
+INSERT INTO `Entry` VALUES (null, "Javascript", "I learned about loops today. They can be a lot of fun.\nI learned about loops today. They can be a lot of fun.\nI learned about loops today. They can be a lot of fun.", "Wed Sep 15 2021 10:10:47 ", 1);
+INSERT INTO `Entry` VALUES (null, "Python", "Python is named after the Monty Python comedy group from the UK. I'm sad because I thought it was named after the snake", "Wed Sep 15 2021 10:11:33 ", 4);
+INSERT INTO `Entry` VALUES (null, "Python", "Why did it take so long for python to have a switch statement? It's much cleaner than if/elif blocks", "Wed Sep 15 2021 10:13:11 ", 3);
+INSERT INTO `Entry` VALUES (null, "Javascript", "Dealing with Date is terrible. Why do you have to add an entire package just to format a date. It makes no sense.", "Wed Sep 15 2021 10:14:05 ", 3);
 
-
-
-
-
-INSERT INTO `Entry` VALUES (null, "Snickers", "Recreation", "Dalmation", 4, 1);
-INSERT INTO `Entry` VALUES (null, "Jax", "Treatment", "Beagle", 1, 1);
-INSERT INTO `Entry` VALUES (null, "Falafel", "Treatment", "Siamese", 4, 2);
-INSERT INTO `Entry` VALUES (null, "Doodles", "Kennel", "Poodle", 3, 1);
-INSERT INTO `Entry` VALUES (null, "Daps", "Kennel", "Boxer", 2, 2);
-INSERT INTO `Entry` VALUES (null, "Cleo", "Kennel", "Poodle", 2, 2);
-INSERT INTO `Entry` VALUES (null, "Popcorn", "Kennel", "Beagle", 3, 2);
-INSERT INTO `Entry` VALUES (null, "Curly", "Treatment", "Poodle", 4, 2);
+SELECT * FROM Mood;
+SELECT * FROM Entry;
